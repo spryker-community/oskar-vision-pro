56f9018be3da6c7ab5268a8ec7a17bce959a33d1
Henk Hornschuh <h.hornschuh@basecom.de>
2024-09-11 17:47:47 +0200

    les try

--- OskarVisionPro/OskarVisionPro/Model3DView.swift
+++ OskarVisionPro/OskarVisionPro/Model3DView.swift
@@ -12,7 +12,8 @@ import SceneKit
 struct Model3DView: View {
     @State private var currentScale: Float = 1.0
     @State private var currentTranslation: SIMD3<Float> = SIMD3<Float>(0, 0, 0)
-    @State private var angle = Angle(degrees: 0.0);
+    @State private var rotationAxis: (x: CGFloat, y: CGFloat, z: CGFloat) = (x: 0.0, y: 0.0, z: 0.0);
+    @State private var rotation = Angle(degrees: 0.0);
     let url: URL
 
     // Replace this with your 3D model's URL
@@ -20,17 +21,24 @@ struct Model3DView: View {
 
     var body: some View {
         ZStack {
-            Model3DViewContainer(url: url, currentScale: $currentScale, currentTranslation: $currentTranslation,
-                angle: $angle
-            )
+            Model3DViewContainer(url: url, currentScale: $currentScale, currentTranslation: $currentTranslation)
                 .gesture(
                     DragGesture(minimumDistance: 0)
                         .onChanged { value in
+                            let angle = sqrt(pow(value.translation.width, 2) + pow(value.translation.height, 2))
+                            rotation = Angle(degrees: Double(angle))
+
+                            let axisX = -value.translation.height / CGFloat(angle)
+                            let axisY = value.translation.width / CGFloat(angle)
+
+                            rotationAxis = (x: axisX, y: axisY, z: 0)
+
                             // Update the translation of the 3D model
                             let translation = SIMD3<Float>(Float(value.translation.width) / 1000, Float(-value.translation.height) / 1000, 0)
                             currentTranslation = translation
                         }
                 )
+                rotation3DEffect(rotation, axis: rotationAxis)
                 .gesture(
                     MagnificationGesture()
                         .onChanged { value in
@@ -46,7 +54,6 @@ struct Model3DViewContainer: View {
     let url: URL
     @Binding var currentScale: Float
     @Binding var currentTranslation: SIMD3<Float>
-    @Binding var angle: Angle;
 
     var body: some View {
         Model3D(url: url)
@@ -59,13 +66,6 @@ struct Model3DViewContainer: View {
             )
             .scaleEffect(CGFloat(currentScale))
             .offset(x: CGFloat(currentTranslation.x), y: CGFloat(currentTranslation.y))
-            .rotationEffect(angle)
-            .gesture(
-                RotateGesture()
-                    .onChanged { value in
-                        angle = value.rotation
-                    }
-            )
             placeholder: do {
                ProgressView()
 
